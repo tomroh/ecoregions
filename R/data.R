5ef890580de1e72dde51a9d103f47f275168c2f2
troh <thmsroh@gmail.com>
2021-01-23 11:50:34 -0800

    init commit

---
+++ R/data.R
@@ -0,0 +1,90 @@
+#' Level 3 Ecoregions of the Continental United States
+#'
+#' The spatial boundaries, color symbology, and classification of ecoregions.
+#'
+#' @usage data(ContinentalUsEcoregion3)
+#'
+#' @format An object of class \code{"sf"} with 1250 rows and 15 variables
+#'
+#' @references \href{ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/us/Eco_Level_III_descriptions.doc}{Description} \href{ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/us/Eco_Level_III_US.html}{Metadata}
+#'
+#' @docType data
+#'
+#' @source \url{https://www.epa.gov/eco-research/level-iii-and-iv-ecoregions-continental-united-states}
+"ContinentalUsEcoregion3"
+
+#' Level 4 Ecoregions of the Continental United States
+#'
+#' The spatial boundaries, color symbology, and classification of ecoregions.
+#'
+#' @usage data(ContinentalUsEcoregion4)
+#'
+#' @format An object of class \code{"sf"} with 5896 rows and 18 variables
+#'
+#' @references \href{ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/us/Eco_Level_IV_US.html}{Metadata}
+#'
+#' @docType data
+#'
+#' @source \url{https://www.epa.gov/eco-research/level-iii-and-iv-ecoregions-continental-united-states}
+"ContinentalUsEcoregion4"
+
+#' Level 1 Ecoregions of North America
+#'
+#' The spatial boundaries, color symbology, and classification of ecoregions.
+#'
+#' @usage data(NorthAmericaEcoregion1)
+#'
+#' @format An object of class \code{"sf"} with 2140 rows and 7 variables
+#'
+#' @references \href{ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/cec_na/NA_CEC_Eco_Level1.htm}{Metadata}
+#'
+#' @docType data
+#'
+#' @source \url{https://www.epa.gov/eco-research/ecoregions-north-america}
+"NorthAmericaEcoregion1"
+
+#' Level 2 Ecoregions of North America
+#'
+#' The spatial boundaries, color symbology, and classification of ecoregions.
+#'
+#' @usage data(NorthAmericaEcoregion2)
+#'
+#' @format An object of class \code{"sf"} with 2261 rows and 10 variables
+#'
+#' @references \href{ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/cec_na/NA_CEC_Eco_Level2.htm}{Metadata}
+#'
+#' @docType data
+#'
+#' @source \url{https://www.epa.gov/eco-research/ecoregions-north-america}
+"NorthAmericaEcoregion2"
+
+#' Level 3 Ecoregions of North America
+#'
+#' The spatial boundaries, color symbology, and classification of ecoregions.
+#'
+#' @usage data(NorthAmericaEcoregion3)
+#'
+#' @format An object of class \code{"sf"} with 2548 rows and 13 variables
+#'
+#' @references \href{ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/pubs/NA_TerrestrialEcoregionsLevel3_Final-2june11_CEC.pdf}{Description} \href{ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/cec_na/NA_CEC_Eco_Level3.htm}{Metadata}
+#'
+#' @docType data
+#'
+#' @source \url{https://www.epa.gov/eco-research/ecoregions-north-america}
+"NorthAmericaEcoregion3"
+
+#' Terrestrial Ecoregions of the World
+#'
+#' The spatial boundaries, color symbology, and classification of ecoregions.
+#'
+#' @usage data(worldecoregions)
+#'
+#' @format An object of class \code{"sf"} with 14458 rows and 25 variables
+#'
+#' @references Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.
+#'
+#' @docType data
+#'
+#' @source \url{https://www.worldwildlife.org/publications/terrestrial-ecoregions-of-the-world}
+"worldecoregions"
+
